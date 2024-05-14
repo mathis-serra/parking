8873e9e9208d41a512ffd9be0b77ce1c355dc8b6
kevin.perettimalaguti <kevin.peretti-malaguti@laplateforme.io>
2024-05-14 16:29:29 +0200

    drawing texte added for menu

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ int main()
     const int screenWidth = 700;
     const int screenHeight = 600;
     const int cellSize = 80;
+    bool displayGrid = false;
 
     InitWindow(screenWidth, screenHeight, "Parking");
     SetTargetFPS(60);
@@ -14,9 +15,26 @@ int main()
 
     while (!WindowShouldClose())
     {
+        if (IsKeyPressed(KEY_ENTER))
+        {
+            displayGrid = !displayGrid;
+        }
+        
         BeginDrawing();
         ClearBackground(WHITE);
-        grid.Draw();
+
+        if (displayGrid)
+        {
+            grid.Draw();
+        }
+        else
+        {
+            int centerX = screenWidth / 2;
+            int centerY = screenHeight / 2;
+
+            DrawText("Press ENTER to PLAY Parking", centerX - MeasureText("Press ENTER to PLAY Parking", 40) / 2, centerY - 60, 40, BLACK);
+            DrawText("Play with directional arrows", centerX - MeasureText("Play with directional arrows", 30) / 2, centerY + 20, 30, BLACK);
+        }
         EndDrawing();
     }
 
